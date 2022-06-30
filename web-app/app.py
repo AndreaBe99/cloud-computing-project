845cad20beba1301831046920a17e260faf21be2
Donato Stanco <dstanco@hotmail.it>
2022-06-30 11:04:06 +0200

    update app.py

--- web-app/app.py
+++ web-app/app.py
@@ -4,14 +4,42 @@ import pandas as pd
 import pickle
 import numpy as np
 
+PATH = "https://raw.githubusercontent.com/AndreaBe99/cloud-computing-project/main/final_all_season.csv"
+
 app = Flask(__name__)
 
 model = pickle.load('../lr_model.pkl')
-cols = ['Div', 'Date', 'HomeTeam', 'AwayTeam', 'B365H', 'B365D', 'B365A']
+cols = ['season', 'Date', 'HomeTeam', 'AwayTeam', 'B365H', 'B365D', 'B365A']
 
 @app.route('/')
 def home():
    return render_template('home.html')
+
+def dataset_manipulation(df):
+    all_season = pd.read_csv(PATH, low_memory=False)
+
+    # Get Rank of current season
+    df['ht_rank'] = np.where(all_season['HomeTeam'] == df['HomeTeam'][0] & all_season['season'] == df['season'][0], all_season['ht_rank'][0])
+    df['at_rank'] = np.where(all_season['AwayTeam'] == df['AwayTeam'][0] & all_season['season'] == df['season'][0], all_season['at_rank'][0])
+
+    # Get Rank of last season
+    df['ht_ls_rank'] = np.where(all_season['HomeTeam'] == df['HomeTeam'][0] & all_season['season'] == df['season'][0], all_season['ht_ls_rank'][0])
+    df['at_ls_rank'] = np.where(all_season['AwayTeam'] == df['AwayTeam'][0] & all_season['season'] == df['season'][0], all_season['at_ls_rank'][0])  
+
+    # Get amount of days since last match
+    last_date = all_season[(all_season['Date'] < df['Date']) & (all_season['season'] == df['season']) & ( (all_season["HomeTeam"] == df['HomeTeam']) | (all_season["AwayTeam"] == df['HomeTeam']) ) ].Date.max()
+    days = (df['Date'] - last_date)/np.timedelta64(1,'D')
+    df['ht_days_ls_match'] = days
+
+    last_date = all_season[(all_season['Date'] < df['Date']) & (all_season['season'] == df['season']) & ( (all_season["HomeTeam"] == df['AwayTeam']) | (all_season["AwayTeam"] == df['AwayTeam']) ) ].Date.max()
+    days = (df['Date'] - last_date)/np.timedelta64(1,'D')
+    df['at_days_ls_match'] = days
+
+    
+
+
+    # Convert Date Column to Datetime
+    df['Date'] = pd.to_datetime(df.Date)
 
 @app.route('/predict',methods=['POST'])
 def predict():
