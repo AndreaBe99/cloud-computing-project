4f52d4a7184bbb9b2eea63fccbf2af920353add2
Andrea Bernini <andreabe99@gmail.com>
2022-07-14 18:08:42 +0200

    Update Dataset Path

--- config.py
+++ config.py
@@ -10,7 +10,9 @@ BUCKET_NAME = 'cc-project-football-bet-bucket-model'
 GOOGLE_APPLICATION_CREDENTIALS = 'cc-project-football-bet-key.json'
 
 # Dataset Github Path
-PATH_DATASET = "https://raw.githubusercontent.com/AndreaBe99/cloud-computing-project/main/src/"
+# PATH_DATASET = "https://raw.githubusercontent.com/AndreaBe99/cloud-computing-project/main/src/"
+# Local
+PATH_DATASET = "src/"
 DATASET = PATH_DATASET + "final_all_season.csv"
 
 # Model Name
